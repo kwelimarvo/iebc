4a830b7cfbb97a4dbc55e04c0f56ed323f242601
kwelimarvo <marvin@kwelicapital.com>
2022-06-22 10:03:20 +0300

    Vue Component Config

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
-require('./bootstrap');
+require('./bootstrap')
+
+import { createApp } from 'vue'
+// import HelloWorld from './components/Welcome'
+import Products from './components/Products'
+
+const app = createApp({
+	data() {
+		return {
+			name: 'Marvin'
+		}
+	}
+})
+
+// app.component('hello-world', HelloWorld)
+app.component('products', Products)
+
+app.mount('#app')
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,35 @@
-<!doctype html>
-<html>
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
-<body>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-</body>
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
+
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+            }
+        </style>
+    </head>
+    <body class="antialiased">
+        <div id="app">
+            <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center py-4 sm:pt-0">
+                <div class="p-3 cursor-pointer rounded-md bg-rose-400">
+                    <!-- <hello-world/> -->
+                    <products img="https://hplussport.com/wp-content/uploads/2016/12/slicker-jacket_LYNDA_29941.jpg" />
+                </div>
+            </div>
+        </div>
+
+        <script src="{{ mix('js/app.js') }}"></script>
+    </body>
 </html>
